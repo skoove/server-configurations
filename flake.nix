e856ceb953e3cc1d6b0773887052fa2f1a9247bd
Zie Sturges <zie@sturges.com.au>
2025-04-11 14:37:56 +1000

    asdsad

--- flake.nix
+++ flake.nix
@@ -4,32 +4,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    catppuccin.url = "github:catppuccin/nix";
   };
 
-  outputs = { self, nixpkgs, home-manager, catppuccin, ... }:
+  outputs = { nixpkgs, ... }:
   let
     system = "x86_64-linux";
     lib = nixpkgs.lib;
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-    homeConfigurations = {
-      zie = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./home.nix
-          catppuccin.homeManagerModules.catppuccin
-        ];
-      };
-    };
-
     nixosConfigurations = {
       # this is just for testing
       nixos-test-server = lib.nixosSystem {
         inherit system;
         modules = [
           ./configuration.nix
-          catppuccin.nixosModules.catppuccin
           ./server-configs/nixos-test-server/configuration.nix
         ];
       };
