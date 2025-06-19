8ab1499af5edc5f0f736c053579aee0c461cf70b
skoove <zie@skoove.dev>
2025-06-20 07:49:50 +1000

    jellyfin: add introskipper plugin

--- server-configs/jellyfin/configuration.nix
+++ server-configs/jellyfin/configuration.nix
@@ -2,6 +2,27 @@
 {
   networking.hostName = "jellyfin";
 
+  nixpkgs.overlays = [
+    (
+      final: prev:
+        {
+          jellyfin-web = prev.jellyfin-web.overrideAttrs (finalAttrs: previousAttrs: {
+            installPhase = ''
+              runHook preInstall
+
+              # this is the important line
+              sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
+
+              mkdir -p $out/share
+              cp -a dist $out/share/jellyfin-web
+
+              runHook postInstall
+            '';
+          });
+        }
+    )
+  ];
+
   fileSystems."/mnt/nas" = {
     device = "192.168.0.230:/volume1/Media";
     fsType = "nfs";
