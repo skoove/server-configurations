16ef823cb79ff52b9cf553d8827c58187ba88ccd
skoove <zie@skoove.dev>
2025-06-19 20:25:19 +1000

    jellyfin enable

--- server-configs/jellyfin/configuration.nix
+++ server-configs/jellyfin/configuration.nix
@@ -1,4 +1,16 @@
 { ... }:
 {
   networking.hostName = "jellyfin";
+
+  fileSystems."/mnt/nas" = {
+    device = "192.168.0.230:/volume1/Media";
+    fsType = "nfs";
+    options = [ "nofail" "x-systemd.automount" "x-systemd.device-timeout=10s" ];
+  };
+
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+    user = "zie";
+  };
 }
