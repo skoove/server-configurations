327e757bd1d6567628fb01b835545cce84093924
skoove <zie@skoove.dev>
2025-06-25 12:13:31 +1000

    enable tailscale on all hosts

--- configuration.nix
+++ configuration.nix
@@ -72,20 +72,11 @@
 
   programs.fish.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.qemuGuest.enable = true;
   services.spice-vdagentd.enable = true;
+  services.tailscale.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
