116415e5c3e3d2850938eff1bd57c5611bc54738
Zie <53106860+skoove@users.noreply.github.com>
2025-07-13 14:07:46 +1000

    disable mutable users

--- configuration.nix
+++ configuration.nix
@@ -66,29 +66,33 @@
     variant = "";
   };
 
-  users.users = {
-    root = {
-      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKO45q8PNR1u3rfs+un5tWSV1vAzewZoT76iB1E+JLh zie@nixos-desktop" ];
-      hashedPassword = "$6$aov1GUFdGTykn2Bu$T3PL3N3I6I5N9639YhsJqKk.gMkoAm2m7tqHhaAaxg8.T4/E6fG8f8pDy5931cNTTifoZz6lpTQuFuNoO98Ql1";
-    };
+  users = {
+    mutableUsers = false;
+
+    users = {
+      root = {
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKO45q8PNR1u3rfs+un5tWSV1vAzewZoT76iB1E+JLh zie@nixos-desktop" ];
+        hashedPassword = "$6$aov1GUFdGTykn2Bu$T3PL3N3I6I5N9639YhsJqKk.gMkoAm2m7tqHhaAaxg8.T4/E6fG8f8pDy5931cNTTifoZz6lpTQuFuNoO98Ql1";
+      };
     
-    zie = {
-      isNormalUser = true;
-      description = "Zie";
-      extraGroups = [ "networkmanager" "wheel" ];
-      hashedPassword = "$6$OlvFrwWdhd1VlHoC$kinv7.fERmkcwrDTg6e9QFFlAiL8Twd3.ljmB3yNLqS1wpb93hNVWJY6jglJYFJG/z/teAAGlqtrWquJX3rM21";
+      zie = {
+        isNormalUser = true;
+        description = "Zie";
+        extraGroups = [ "networkmanager" "wheel" ];
+        hashedPassword = "$6$OlvFrwWdhd1VlHoC$kinv7.fERmkcwrDTg6e9QFFlAiL8Twd3.ljmB3yNLqS1wpb93hNVWJY6jglJYFJG/z/teAAGlqtrWquJX3rM21";
 
-      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKO45q8PNR1u3rfs+un5tWSV1vAzewZoT76iB1E+JLh zie@nixos-desktop" ];
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKO45q8PNR1u3rfs+un5tWSV1vAzewZoT76iB1E+JLh zie@nixos-desktop" ];
+      };
     };
-  };
 
-  security.sudo.wheelNeedsPassword = true;
+    security.sudo.wheelNeedsPassword = true;
 
-  users.defaultUserShell = pkgs.fish;
+    users.defaultUserShell = pkgs.fish;
 
-  environment.sessionVariables = {
-    EDITOR = "hx";
-    COLORTERM = "truecolor";
+    environment.sessionVariables = {
+      EDITOR = "hx";
+      COLORTERM = "truecolor";
+    };
   };
 
   # Allow unfree packages
