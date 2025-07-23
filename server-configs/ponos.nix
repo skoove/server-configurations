159fd444cc6af77ad7aadc3f6e866261665477fe
skoove <zie@skoove.dev>
2025-07-23 10:50:28 +1000

    change tld to home.com

--- server-configs/ponos.nix
+++ server-configs/ponos.nix
@@ -16,8 +16,8 @@
   services.caddy = {
     enable = true;
     virtualHosts = {
-      "proxmox-node-1.home".extraConfig = ''reverse_proxy https://192.168.0.230:8006'';
-      "proxmox-node-2.home".extraConfig = ''reverse_proxy https://192.168.0.231:8006'';
+      "proxmox-node-1.home.com".extraConfig = ''reverse_proxy https://192.168.0.230:8006'';
+      "proxmox-node-2.home.com".extraConfig = ''reverse_proxy https://192.168.0.231:8006'';
     };
   };
 }
