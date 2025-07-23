ab8b2c04fde80c9c338f2069cec7aff6d577d78a
Zie <53106860+skoove@users.noreply.github.com>
2025-07-23 17:49:08 +1000

    stuff

--- server-configs/ponos.nix
+++ server-configs/ponos.nix
@@ -13,15 +13,32 @@
     openFirewall = true;
   };
 
+  # caddy firewall ports
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
   services.caddy = {
     enable = true;
-    virtualHosts = {
-      "proxmox-node-1.home.com:80".extraConfig = ''
-        reverse_proxy https://192.168.0.230:8006 {
-          header_up Host {upstream_hostport}
-        }
-      '';
-      "proxmox-node-2.home.com:80".extraConfig = ''reverse_proxy https://192.168.0.231:8006'';
-    };
+    virtualHosts = let
+      mkReverseProxy = {domain, ip}: {
+        ${domain} = {
+          extraConfig = ''
+            tls internal
+            reverse_proxy ${ip} {
+              transport http {
+                tls
+                tls_insecure_skip_verify
+              }
+            }
+          '';
+        };
+      };
+
+      proxies = [
+        { domain = "proxmox-1.home.com"; ip = "192.168.0.230:8006"; }
+        { domain = "proxmox-2.home.com"; ip = "192.168.0.231:8006"; }
+        { domain = "jellyfin.home.com"; ip = "localhost:8096"; }
+        { domain = "transmission.home.com"; ip = "192.168.0.211:9091"; }
+      ];
+    in
+      builtins.foldl' (acc: val: acc // mkReverseProxy val) {} proxies;
   };
 }
