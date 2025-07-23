3577b20e409ecffe47275f4c6f15be5870ac6c99
Zie <53106860+skoove@users.noreply.github.com>
2025-07-23 10:36:32 +1000

    stuff

--- server-configs/ponos.nix
+++ server-configs/ponos.nix
@@ -12,4 +12,12 @@
     enable = true;
     openFirewall = true;
   };
+
+  services.caddy = {
+    enable = true;
+    virtualHosts = {
+      "proxmox-node-1.home".extraConfig = ''reverse_proxy https://192.168.0.230:8006'';
+      "proxmox-node-2.home".extraConfig = ''reverse_proxy https://192.168.0.231:8006'';
+    };
+  };
 }
