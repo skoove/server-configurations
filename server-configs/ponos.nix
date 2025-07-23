660753edddf51e8a291e8516cfe8d702abb6e8a7
Zie <53106860+skoove@users.noreply.github.com>
2025-07-23 11:29:34 +1000

    try thing

--- server-configs/ponos.nix
+++ server-configs/ponos.nix
@@ -16,8 +16,12 @@
   services.caddy = {
     enable = true;
     virtualHosts = {
-      "proxmox-node-1.home.com".extraConfig = ''reverse_proxy https://192.168.0.230:8006'';
-      "proxmox-node-2.home.com".extraConfig = ''reverse_proxy https://192.168.0.231:8006'';
+      "proxmox-node-1.home.com:80".extraConfig = ''
+        reverse_proxy https://192.168.0.230:8006 {
+          header_up Host {upstream_hostport}
+        }
+      '';
+      "proxmox-node-2.home.com:80".extraConfig = ''reverse_proxy https://192.168.0.231:8006'';
     };
   };
 }
