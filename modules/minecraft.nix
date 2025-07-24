82ef7843e0665f749a75e0962f0d0848af1d1de8
skoove <zie@skoove.dev>
2025-07-24 14:50:59 +1000

    init minecraft

---
+++ modules/minecraft.nix
@@ -0,0 +1,39 @@
+{ pkgs , inputs }:
+{
+  imports = [
+    inputs.nix-minecraft.nixosModules.minecraft-servers
+  ];
+
+  nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
+
+  services.minecraft-servers = {
+    enable = true;
+    eula = true;
+    openFirewall = true;
+    servers."vannila-ish" = {
+      enable = true;
+      autostart = true;
+      enableReload = true;
+      jvmOpts = "-Xmx4G -Xms4G";
+      package = pkgs.fabricServers.fabric-1_21_8;
+      serverProperties = {
+        motd = "nixos server lets goooo";
+        online-mode = true;
+      };
+      symlinks = {
+        operators = pkgs.writeTextFile {
+          name = "ops.json";
+          text = ''
+            [
+              {
+                "uuid": "442b7f11-e4f8-46f1-ace0-79b5e810659d",
+                "name": "Zidget_",
+                "level": 4,
+              },
+            ]
+          '';
+        };
+      };
+    };
+  };
+}
