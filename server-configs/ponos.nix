3031f943a0f1a25930179b3890ed6f5e0b632987
Zie <53106860+skoove@users.noreply.github.com>
2025-08-01 15:15:24 +1000

    fuix

--- server-configs/ponos.nix
+++ server-configs/ponos.nix
@@ -33,7 +33,7 @@
       CREATE_ADMIN = 0;
       FETCH_NEBULA_WATCH_TIME = 1;
       FETCH_ODYSEE_WATCH_TIME = 1;
-      LISTEN_ADDRR = "0.0.0.0:7000";
+      LISTEN_ADDR = "0.0.0.0:7000";
     };
   };
 
