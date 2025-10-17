544b453b3b498dc7e0955aec469f057c6ba689ee
skoove <zie@skoove.dev>
2025-10-17 16:23:31 +1000

    minecraft: remove superflat server

--- modules/minecraft.nix
+++ modules/minecraft.nix
@@ -44,37 +44,5 @@
         };
       };
     };
-    
-    servers."supersuperflatish" = {
-      enable = true;
-      autoStart = true;
-      enableReload = true;
-      jvmOpts = "-Xmx2G -Xms2G";
-      package = pkgs.fabricServers.fabric-1_21_8;
-      serverProperties = {
-        motd = "how did u see this";
-        online-mode = false;
-        spawn-protection = 0;
-        difficulty = "hard";
-        generate-structures = false;
-        level-type = "minecraft\:flat";
-        server-port = 25566;
-      };
-
-      symlinks = {
-        "ops.json" = pkgs.writeTextFile {
-          name = "operators-list";
-          text = ''
-            [
-              {
-                "uuid": "b863a2f8-d6b9-3df6-b3b8-b0a1a590c724",
-                "name": "Zidget_",
-                "level": 4
-              }
-            ]
-          '';
-        };
-      };
-    };
   };
 }
